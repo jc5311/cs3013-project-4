b8bea98efa8411d7e094b430bb80ffff558e7ce9
JuanC <jc.psp5@gmail.com>
2018-10-06 12:54:24 -0400

    Makefile fixed. Also implemented use of mmap to read in a file to a memory map fr sub string searching.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
-default: proj4.o
+all: proj4.o
 	g++ proj4.o -o proj4
 
-proj4.o:
+proj4.o: proj4.cpp
 	g++ -c proj4.cpp
 
 clean:
--- proj4.cpp
+++ proj4.cpp
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/mman.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
@@ -16,7 +17,6 @@ using namespace std;
 //global vars
 char *filename;
 char *search_string;
-char read_buffer[BYTESTOREAD];
 int bytes_to_read = DEFAULTBYTESTOREAD;
 
 int main(int argc, char* argv[]){
@@ -25,8 +25,7 @@ int main(int argc, char* argv[]){
 	//read the file and find the number of instances of a string byte by byte of
 	// the given search string
 	int fd;
-	int mmap = 0;
-
+	int do_mmap = 0;
 	//check the input
 	if (argc < 3){
 		//if the minimum number of arguments was not met
@@ -41,22 +40,23 @@ int main(int argc, char* argv[]){
 
 	if (argc > 3){
 		//third argument given
-		if ( strcmp("mmap", argv[4]) == 0){
+		if ( strcmp("mmap", argv[3]) == 0){
 			//mmap entered, acknowledge this
-			mmap = 1;
+			do_mmap = 1;
 		}
 		else{
 			//mmap not entered, assume it is a number
-			int entry = atoi(argv[4]);
+			int entry = atoi(argv[3]);
 			if (entry >= MAXCHUNK){
 				bytes_to_read = MAXCHUNK;
 			}
-			else if ( (entry > 0) || (entry < MAXCHUNK)){
+			else if ( (entry > 0) && (entry < MAXCHUNK)){
 				bytes_to_read = entry;
 			}
 		}
 	}
 
+	char read_buffer[bytes_to_read];
 	//open the file
 	errno = 0;
 	if ( (fd = open(filename, O_RDONLY)) < 0){
@@ -76,31 +76,43 @@ int main(int argc, char* argv[]){
 	//read the file
 	int numof_bytes_read;
 	int count = 0;
-	while ( (numof_bytes_read = read(fd, read_buffer, BYTESTOREAD)) > 0){
-		//read returns the number of bytes read and increments some counter
-		//that marks where it currently is. So loop the read function and check
-		//what it stores in read_buffer for words to count. Incremement a count
-		//for the number of times a search string is found
 
-
-		//go through read_buffer and count num of search_string gound
-		char *ptr = strstr(read_buffer, search_string);
-		while (ptr != NULL){
-			count++;
+	if (do_mmap){
+		errno = 0;
+		void *mmapped_data = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+		if (mmapped_data < 0){
+			cout << "mmap error: errno set to " << errno << endl;
+			return 1;
+		}
+		//read through the returned area for the string
+		char *ptr = strstr( (char*)mmapped_data, search_string);
+		while ( ptr != NULL){
+			count ++;
 			ptr = strstr(ptr + 1, search_string);
 		}
 	}
+	else{
+		while ( (numof_bytes_read = read(fd, read_buffer, bytes_to_read)) > 0){
+			//read returns the number of bytes read and increments some counter
+			//that marks where it currently is. So loop the read function and check
+			//what it stores in read_buffer for words to count. Incremement a count
+			//for the number of times a search string is found
 
-	cout << "num of substr found " << count << endl;;
+
+			//go through read_buffer and count num of search_string gound
+			char *ptr = strstr(read_buffer, search_string);
+			while (ptr != NULL){
+				count++;
+				ptr = strstr(ptr + 1, search_string);
+			}
+		}
+	}
+
 	if (numof_bytes_read < 0){
 		cerr << "read error:";
 		return 1;
 	}
-
-
-
-
-
-
+	cout << "num of substr found " << count << endl;;
+	
 	return 0;
 } //end of main
